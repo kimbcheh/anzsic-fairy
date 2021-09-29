7f34677ae72a34523f48eb29bbb75d3a2a5ade27
Kimberly <83323970+kimbcheh@users.noreply.github.com>
2021-09-29 10:48:14 +1000

    feat: setup package dependencies  (#1)
    * add .prettierrc config file
    
    * remove create-react-app default content
    
    * add grommet package dependencies
    
    * add axios package dependency
    
    * add grommet theme and google font import

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Grommet } from 'grommet'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const theme = {
+ global: {
+  font: {
+   family: 'Poppins',
+  },
+ },
 }
 
-export default App;
+function App() {
+ return (
+  <Grommet theme={theme}>
+   <p>ANZSIC Fairy</p>
+  </Grommet>
+ )
+}
+
+export default App
