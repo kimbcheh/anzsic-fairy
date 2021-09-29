81310bd9d60474e062e33f74bb907e6b42d3d744
Kimberly <kimbcheh@gmail.com>
2021-09-29 11:42:31 +1000

    center heading inside header

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -2,7 +2,7 @@ import { Header, Heading } from 'grommet'
 
 function MainHeader() {
  return (
-  <Header background='brand' height='xsmall' pad='medium'>
+  <Header background='brand' direction='column'>
    <Heading size='small'>&#129498; ANZSIC Fairy</Heading>
   </Header>
  )
