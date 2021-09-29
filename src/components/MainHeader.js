161d927adcde21612baa73502789cf13bc9a3072
Kimberly <kimbcheh@gmail.com>
2021-09-29 11:17:57 +1000

    add header and heading

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,8 +1,10 @@
+import { Header, Heading } from 'grommet'
+
 function MainHeader() {
  return (
-  <div>
-   <p>Header</p>
-  </div>
+  <Header background='brand' height='xsmall' pad='medium'>
+   <Heading size='small'>&#129498; ANZSIC Fairy</Heading>
+  </Header>
  )
 }
 
