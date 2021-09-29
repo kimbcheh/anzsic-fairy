3d31fd0db9c9e68840791ff77e730e97c3ac3024
Kimberly <83323970+kimbcheh@users.noreply.github.com>
2021-09-29 12:04:11 +1000

    feat: add header component (#2)
    * add main component
    
    * add header component
    
    * rename header component
    
    * add header and heading
    
    * center heading inside header

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import MainHeader from './MainHeader'
+
+function Main() {
+ return (
+  <div>
+   <MainHeader />
+  </div>
+ )
+}
+
+export default Main
---
+++ src/components/MainHeader.js
@@ -0,0 +1,11 @@
+import { Header, Heading } from 'grommet'
+
+function MainHeader() {
+ return (
+  <Header background='brand' direction='column'>
+   <Heading size='small'>&#129498; ANZSIC Fairy</Heading>
+  </Header>
+ )
+}
+
+export default MainHeader
