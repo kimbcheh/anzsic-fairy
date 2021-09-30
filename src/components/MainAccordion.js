6e9fb4dd0f9d0e47c2d61974b91d0aff043431d0
Kimberly <kimbcheh@gmail.com>
2021-09-30 12:46:06 +1000

    add accordion and panel components

--- src/components/MainAccordion.js
+++ src/components/MainAccordion.js
@@ -1,14 +1,31 @@
-import { Box, Text } from 'grommet'
+import { Accordion, AccordionPanel, Box, Text } from 'grommet'
 
 function MainAccordion() {
  return (
-  <Box
-   margin='large'
-   direction='row-responsive'
-   justify='center'
-   align='center'
-  >
+  <Box margin='medium' direction='column' justify='center' align='center'>
    <Text size='xlarge'>Frequently Asked Questions</Text>
+   <Accordion fill='true'>
+    <AccordionPanel label='Is ANZSIC Fairy an official tool?'>
+     <Box pad='small'>
+      <Text>X</Text>
+     </Box>
+    </AccordionPanel>
+    <AccordionPanel label='What does ANZSIC even stand for?'>
+     <Box pad='small'>
+      <Text>X</Text>
+     </Box>
+    </AccordionPanel>
+    <AccordionPanel label='Why does ANZSIC exist?'>
+     <Box pad='small'>
+      <Text>X</Text>
+     </Box>
+    </AccordionPanel>
+    <AccordionPanel label='How do I decide which ANZSIC to use?'>
+     <Box pad='small'>
+      <Text>X</Text>
+     </Box>
+    </AccordionPanel>
+   </Accordion>
   </Box>
  )
 }
